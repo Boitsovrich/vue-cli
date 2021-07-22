d58098ad45c6739a06ca602996b6325ff0c72222
aboytsov <aboytsov@phoenixit.ru>
2021-07-22 12:30:29 +0300

    connection scss

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="wrapper">
+    <!-- <header></header> -->
+
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <h1>HELLO</h1>
+          <img alt="Vue logo" src="./assets/logo.png" />
+        </div>
+      </section>
+    </div>
+
+    <!-- <footer></footer> -->
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
